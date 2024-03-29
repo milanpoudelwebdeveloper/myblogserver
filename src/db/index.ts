a58778be291fbdd9e3dd609bbbe048b0e0ff6d25
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-03-29 12:19:22 +0545

    Change ssl pg

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,10 @@
 import pkg from 'pg'
 const { Pool } = pkg
-const pool = new Pool({})
+const pool = new Pool({
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 export default {
   query: (text: string, params: string[]) => pool.query(text, params)
