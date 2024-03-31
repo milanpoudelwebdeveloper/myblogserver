19a0c38b330c91230dd56205698cf204a91977f3
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-03-31 20:28:43 +0545

    Feat: Add ssl config in pg

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,10 @@
 import pkg from 'pg'
 const { Pool } = pkg
-const pool = new Pool({})
+const pool = new Pool({
+  ssl: {
+    rejectUnauthorized: process.env.NODE_ENV ? true : false
+  }
+})
 
 export default {
   query: (text: string, params: string[]) => pool.query(text, params)
