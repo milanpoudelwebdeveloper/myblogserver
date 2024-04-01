623bcc84c4c2ad468f1732f1be03192d71491dae
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-01 19:31:48 +0545

    add origins

--- src/server.ts
+++ src/server.ts
@@ -15,14 +15,12 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use((_: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://codewithmilan.com')
+  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://codewithmilan.com'])
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
-
   next()
 })
-console.log('they running')
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
