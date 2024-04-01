97bd09638d04987f4db3670a0b5e2404571ab7e2
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-01 09:17:08 +0545

    Feat: Add ssl reject unauthorized

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,11 @@
 import pkg from 'pg'
 const { Pool } = pkg
 
-const pool = new Pool()
+const pool = new Pool({
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
 
 export default {
   query: (text: string, params: string[]) => pool.query(text, params)
