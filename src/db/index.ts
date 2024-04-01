f3c61147fa5872d2b1cc7fa93ec58268d1695782
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-01 11:37:13 +0545

    add ssl

---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import pkg from 'pg'
+const { Pool } = pkg
+
+const pool = new Pool({
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+export default {
+  query: (text: string, params: string[]) => pool.query(text, params)
+}
