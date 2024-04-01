dc3899d94ee4bfa2d2f960902fee5d3e46cfed8c
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-01 09:06:05 +0545

    Feat: Add cors origin in array

--- src/server.ts
+++ src/server.ts
@@ -25,7 +25,7 @@ app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(
   cors({
-    origin: 'https://www.codewithmilan.com' || 'http://localhost:3000',
+    origin: [process.env.CORS_ORIGIN!, 'http://localhost:3000'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   })
