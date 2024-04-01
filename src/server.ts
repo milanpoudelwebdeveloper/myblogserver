92608e6d86188a23e40fa1499689f51a65296cb1
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-01 20:27:38 +0545

    change

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import blogRoutes from '@routes/blog'
 import authRoutes from '@routes/auth'
 import userRoutes from '@routes/user'
 import stats from '@routes/stats'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -14,10 +15,15 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://codewithmilan.com'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+  })
+)
+
 app.use((_: Request, res: Response, next: NextFunction) => {
-  res.setHeader('Access-Control-Allow-Origin', ['http://localhost:3000', 'https://codewithmilan.com'])
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
   next()
 })
