519979c3235a605245d81b5bd4dea4ebbf73225b
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-02 09:39:09 +0545

    modify config

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from 'express'
+import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
 import morgan from 'morgan'
@@ -7,7 +7,6 @@ import blogRoutes from '@routes/blog'
 import authRoutes from '@routes/auth'
 import userRoutes from '@routes/user'
 import stats from '@routes/stats'
-import cors from 'cors'
 
 dotenv.config()
 
@@ -15,21 +14,27 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(
-  cors({
-    origin: 'https://codewithmilan.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  })
-)
+// app.use(
+//   cors({
+//     origin: 'https://codewithmilan.com',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true
+//   })
+// )
 
-app.use((_: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Origin', 'https://codewithmilan.com')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT ,DELETE')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+app.use(function (req, res, next) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ['http://localhost:3000', 'https://codewithmilan.com', 'https://www.codewithmilan.com']
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin as string)) {
+    res.setHeader('Access-Control-Allow-Origin', origin as string)
+  }
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  res.header('Access-Control-Allow-credentials', true.toString())
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   next()
 })
+
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
