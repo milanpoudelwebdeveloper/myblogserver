3a6891bac40b6440630d0ec292f28fdb376a0408
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-02 10:05:51 +0545

    modify configs no

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import blogRoutes from '@routes/blog'
 import authRoutes from '@routes/auth'
 import userRoutes from '@routes/user'
 import stats from '@routes/stats'
+import cors from 'cors'
 
 dotenv.config()
 
@@ -14,26 +15,13 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-// app.use(
-//   cors({
-//     origin: 'https://codewithmilan.com',
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-//   })
-// )
-
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ['http://localhost:3000', 'https://codewithmilan.com', 'https://www.codewithmilan.com']
-  const origin = req.headers.origin
-  if (allowedOrigins.includes(origin as string)) {
-    res.setHeader('Access-Control-Allow-Origin', origin as string)
-  }
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.header('Access-Control-Allow-credentials', true.toString())
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  next()
-})
+app.use(
+  cors({
+    origin: 'https://codewithmilan.com',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  })
+)
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
