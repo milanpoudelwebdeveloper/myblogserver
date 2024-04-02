e62e912e837ea47ba7ea8c7867ac9267cd715656
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-02 11:33:22 +0545

    modify configs no

--- src/server.ts
+++ src/server.ts
@@ -15,21 +15,8 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ credentials: true, origin: 'https://www.codewithmilan.com' }))
+app.use(cors({ credentials: true, origin: 'https://www.codewithmilan.com', optionsSuccessStatus: 200 }))
 
-app.use(function (req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  // const allowedOrigins = ['http://localhost:3000', 'https://codewithmilan.com', 'https://www.codewithmilan.com']
-  // const origin = req.headers.origin
-  // if (allowedOrigins.includes(origin as string)) {
-  //   res.setHeader('Access-Control-Allow-Origin', origin as string)
-  // }
-  res.header('Access-Control-Allow-Origin', 'https://www.codewithmilan.com')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-  res.header('Access-Control-Allow-credentials', true)
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  next()
-})
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
