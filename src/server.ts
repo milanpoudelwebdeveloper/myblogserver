270a3e18903a10b96770182d2a7e7a7820aea795
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-02 12:17:43 +0545

    modify configs nokkhh

--- src/server.ts
+++ src/server.ts
@@ -20,14 +20,7 @@ const allowedOrigins = ['http://localhost:3000', 'https://www.codewithmilan.com'
 app.use(
   cors({
     credentials: true,
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true)
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
-        return callback(new Error(msg), false)
-      }
-      return callback(null, true)
-    },
+    origin: allowedOrigins,
     optionsSuccessStatus: 200,
     methods: 'GET, POST, PUT, DELETE'
   })
