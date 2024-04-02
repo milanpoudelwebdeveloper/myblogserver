8356a1171d9ca1bf35239a96874d2c6769333505
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-02 11:40:32 +0545

    modify configs no

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,22 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
-app.use(cors({ credentials: true, origin: 'https://www.codewithmilan.com', optionsSuccessStatus: 200 }))
+const allowedOrigins = ['http://localhost:3000', 'https://www.codewithmilan.com']
+
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = 'The CORS policy for this site does not allow access from the specified Origin.'
+        return callback(new Error(msg), false)
+      }
+      return callback(null, true)
+    },
+    optionsSuccessStatus: 200
+  })
+)
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
