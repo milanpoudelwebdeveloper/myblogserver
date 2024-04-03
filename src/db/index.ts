789c83994549e4293ef4bd55ad9614020046f787
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-03 09:29:20 +0545

    few pg config changes

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,18 +1,21 @@
 import pkg from 'pg'
 const { Pool } = pkg
+import dotenv from 'dotenv'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-let pool: any
+dotenv.config()
 
-if (process.env.NODE_ENV) {
-  pool = new Pool({})
-} else {
-  pool = new Pool({
-    ssl: {
-      rejectUnauthorized: false
-    }
-  })
-}
+const pool = (() => {
+  if (process.env.NODE_ENV === 'development') {
+    return new Pool({})
+  } else {
+    return new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+    })
+  }
+})()
 
 export default {
   query: (text: string, params: string[]) => pool.query(text, params)
