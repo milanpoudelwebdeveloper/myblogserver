297f7b3194a7d4fed8dce43aadd946c8d3ddf978
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-10 07:49:12 +0545

    Remove unnecessary code

--- src/server.ts
+++ src/server.ts
@@ -8,14 +8,10 @@ import authRoutes from '@routes/auth'
 import userRoutes from '@routes/user'
 import stats from '@routes/stats'
 import cors from 'cors'
-import bcrypt from 'bcrypt'
-
 dotenv.config()
 
 const app = express()
-
 const PORT = process.env.PORT || 5000
-
 const allowedOrigins = ['http://localhost:3000', 'https://www.codewithmilan.com', 'https://codewithmilan.com']
 
 app.use(
@@ -31,20 +27,12 @@ app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(morgan('dev'))
-
 app.set('trust proxy', 1)
 app.use('/api/category', categoryRoutes)
 app.use('/api/blog', blogRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/stats', stats)
-
-const generatePassword = async () => {
-  const salt = await bcrypt.genSalt(10)
-  const hashedPassword = bcrypt.hashSync('Orange.4513@', salt)
-  console.log('hashedPassword', hashedPassword)
-}
-generatePassword()
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
