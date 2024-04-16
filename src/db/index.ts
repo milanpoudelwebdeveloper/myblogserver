9e0e122c932d75a77123ce674c946d2a223b6290
Milan Poudel <milanwebdeveloper1@gmail.com>
2024-04-16 17:33:51 +0545

    change in node.js.yml

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,22 +4,11 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-// PGUSER=postgres
-// PGHOST=localhost
-// PGPASSWORD=milanpoudel
-// PGDATABASE=myblogserver
-// PGPORT=5432
-
 const pool = (() => {
   if (process.env.NODE_ENV === 'development') {
     return new Pool({})
   } else {
     return new Pool({
-      user: 'postgres.nilyjfpmxpoaeqcblmip',
-      host: 'aws-0-ap-south-1.pooler.supabase.com',
-      database: 'postgres',
-      password: 'thebestdeveloper_1234',
-      port: 5432,
       ssl: {
         rejectUnauthorized: false
       }
